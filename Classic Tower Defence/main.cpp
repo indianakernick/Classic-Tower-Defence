bb64af1c5b5fa7828838b59fa182b40f48db552a
Kerndog73 <kerndog73@gmail.com>
2018-04-21 09:54:57 +0930

    Setup Project
    I really should use a project template to do this.

---
+++ Classic Tower Defence/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Classic Tower Defence
+//
+//  Created by Indi Kernick on 21/4/18.
+//  Copyright © 2018 Indi Kernick. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int, char **) {
+  std::cout << "Hello world\n";
+  return 0;
+}
