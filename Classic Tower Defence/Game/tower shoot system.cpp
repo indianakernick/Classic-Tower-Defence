e6318851970f7cd94b799834ed7f33bda99f127a
Kerndog73 <kerndog73@gmail.com>
2018-04-21 15:39:31 +0930

    Add tower shoot system

---
+++ Classic Tower Defence/Game/tower shoot system.cpp
@@ -0,0 +1,34 @@
+//
+//  tower shoot system.cpp
+//  Classic Tower Defence
+//
+//  Created by Indi Kernick on 21/4/18.
+//  Copyright © 2018 Indi Kernick. All rights reserved.
+//
+
+#include "tower shoot system.hpp"
+
+#include "unit stats component.hpp"
+#include "tower stats component.hpp"
+#include "tower timing component.hpp"
+#include "tower target component.hpp"
+
+void towerShootSystem(ECS::Registry &registry) {
+  auto view = registry.view<TowerStats, TowerTiming, TowerTarget>();
+  
+  for (const ECS::EntityID entity : view) {
+    const TowerStats towerStats = view.get<TowerStats>(entity);
+    float &timeSinceLastShot = view.get<TowerTiming>(entity).timeSinceLastShot;
+    if (timeSinceLastShot < 1.0f / towerStats.rof) {
+      continue;
+    }
+    
+    const ECS::EntityID target = view.get<TowerTarget>(entity).id;
+    if (target == ECS::NULL_ENTITY) {
+      continue;
+    }
+    
+    registry.get<UnitStats>(target).health -= towerStats.damage;
+    timeSinceLastShot = 0;
+  }
+}
---
+++ Classic Tower Defence/Game/tower shoot system.hpp
@@ -0,0 +1,16 @@
+//
+//  tower shoot system.hpp
+//  Classic Tower Defence
+//
+//  Created by Indi Kernick on 21/4/18.
+//  Copyright © 2018 Indi Kernick. All rights reserved.
+//
+
+#ifndef tower_shoot_system_hpp
+#define tower_shoot_system_hpp
+
+#include <Simpleton/ECS/registry.hpp>
+
+void towerShootSystem(ECS::Registry &);
+
+#endif
