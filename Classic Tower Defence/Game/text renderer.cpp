faab30b3e1812e95c3e6830314e0ed0528051839
Kerndog73 <kerndog73@gmail.com>
2018-05-02 19:30:34 +0930

    Add TextRenderer
    This will be used for part of the UI. I still have to test it.

---
+++ Classic Tower Defence/Game/text renderer.cpp
@@ -0,0 +1,85 @@
+//
+//  text renderer.cpp
+//  Classic Tower Defence
+//
+//  Created by Indi Kernick on 2/5/18.
+//  Copyright © 2018 Indi Kernick. All rights reserved.
+//
+
+#include "text renderer.hpp"
+
+void TextRenderer::setInfo(const GlyphInfo newInfo) {
+  info = newInfo;
+}
+
+void TextRenderer::setCursor(const glm::vec2 newPos) {
+  beginning = newPos;
+  pos = newPos;
+}
+
+void TextRenderer::resetCursor() {
+  pos = beginning;
+}
+
+void TextRenderer::setScale(const float newScale) {
+  scale = newScale;
+}
+
+void TextRenderer::setDepth(const float newDepth) {
+  depth = newDepth;
+}
+
+void TextRenderer::startLine() {
+  firstChar();
+  nextLine();
+}
+
+void TextRenderer::nextLine() {
+  pos.y += info.advance.y;
+}
+
+void TextRenderer::nextChar() {
+  pos.x += info.advance.x;
+}
+
+void TextRenderer::firstChar() {
+  pos.x = beginning.x;
+}
+
+void TextRenderer::pushChar(
+  G2D::QuadWriter &writer,
+  const Sprite::Sheet &sheet,
+  const char c
+) {
+  if (c == ' ') {
+    return nextChar();
+  }
+  if (c == '\n') {
+    return startLine();
+  }
+  if (!std::isprint(c)) {
+    return;
+  }
+  
+  writer.quad();
+  writer.depth(depth);
+  writer.tilePos(pos, scale * info.size);
+  // the spritesheet is expected to hold all of the printable ascii characters.
+  // std::isprint considers space a printable character but there's no point
+  // in storing a blank space in a sprite sheet. So sheet.getSprite(0) should
+  // return the rectangle for '!' (the character after space)
+  writer.tileTex(sheet.getSprite(c - '!'));
+  
+  nextChar();
+}
+
+void TextRenderer::pushText(
+  G2D::QuadWriter &writer,
+  const Sprite::Sheet &sheet,
+  const std::string_view text
+) {
+  writer.sectionSize(text.size());
+  for (const char c : text) {
+    pushChar(writer, sheet, c);
+  }
+}
---
+++ Classic Tower Defence/Game/text renderer.hpp
@@ -0,0 +1,63 @@
+//
+//  text renderer.hpp
+//  Classic Tower Defence
+//
+//  Created by Indi Kernick on 2/5/18.
+//  Copyright © 2018 Indi Kernick. All rights reserved.
+//
+
+#ifndef text_renderer_hpp
+#define text_renderer_hpp
+
+#include <Simpleton/Sprite/sheet.hpp>
+#include <Simpleton/Graphics 2D/quad writer.hpp>
+
+struct GlyphInfo {
+  glm::vec2 size;
+  glm::vec2 advance;
+};
+
+class TextRenderer {
+public:
+  TextRenderer() = default;
+  
+  void setInfo(GlyphInfo);
+  
+  /// Change the position of the cursor. The x-coordinate of this position
+  /// becomes the start of the line.
+  void setCursor(glm::vec2);
+  /// Return the cursor to the last place that it was set
+  void resetCursor();
+  /// Set the glyph scale (screen_size = glyph_scale * tex_size)
+  void setScale(float);
+  /// Set the depth of the quads
+  void setDepth(float);
+  
+  /// Move the cursor down to the next line and to the start of the line
+  void startLine();
+  /// Move the cursor down to the next line without moving horizontally
+  void nextLine();
+  /// Move the cursor right to the next character
+  void nextChar();
+  /// Move the cursor to the first character of the current line
+  void firstChar();
+  
+  /// Render a single character. Only renders printable characters.
+  /// Non-printable characters (except for '\n' and ' ') are ignored.
+  void pushChar(G2D::QuadWriter &, const Sprite::Sheet &, char);
+  /// Render a string of characters. Calls pushChar for each character.
+  void pushText(G2D::QuadWriter &, const Sprite::Sheet &, std::string_view);
+  
+private:
+  // the last position that was set with a call to setCursor
+  glm::vec2 beginning {0.0f};
+  // the position where the next character will be drawn
+  glm::vec2 pos {0.0f};
+  GlyphInfo info {{1.0f, 1.0f}, {1.0f, 1.0f}};
+  // a scale of one will mean that one pixel on the texture will coorespond
+  // to one pixel on the monitor
+  float scale = 1.0f;
+  float depth = 0.0f;
+};
+
+#endif
