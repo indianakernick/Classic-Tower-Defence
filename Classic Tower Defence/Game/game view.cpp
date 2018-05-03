9a68aada1c204867aaac51f9d3e71dc00dfb05e1
Kerndog73 <kerndog73@gmail.com>
2018-05-03 11:28:56 +0930

    Use RGBA font texture
    It's this or I write a shader for G2D::Renderer just for this use case.

--- Classic Tower Defence/Game/game view.cpp
+++ Classic Tower Defence/Game/game view.cpp
@@ -44,7 +44,7 @@ void GameView::init(G2D::Renderer &renderer) {
 void GameView::playSounds(ECS::Registry &reg) {
   towerSoundSystem(reg, sounds);
   unitDeathSoundSystem(reg, sounds);
-  sounds.play(DupSound::PLAY_LATER);
+  //sounds.play(DupSound::PLAY_LATER);
 }
 
 void GameView::updateCam(const float aspect, const float delta) {
@@ -69,4 +69,6 @@ void GameView::render(ECS::Registry &reg, G2D::QuadWriter &writer) {
   writer.section({textCam.transform.toPixels(), textSheetTex.tex()});
   textRenderer.resetCursor();
   textRenderer.pushText(writer, textSheetTex.sheet(), "I created a text renderer!\nIsn't it cool!");
+  textRenderer.startLine();
+  textRenderer.pushText(writer, textSheetTex.sheet(), "Some text that renders over the game");
 }
