4e2ae9c8769065ed13a1618f6f7f9de1f342c64f
Kerndog73 <kerndog73@gmail.com>
2018-05-05 10:34:02 +0930

    Draw a laser
    The laser fires a continuous beam at the units. The laser beam sprite will be stretched from the tower to the unit. For this we'll need a rendering system for projectiles and a rendering system for beams.

---
+++ Classic Tower Defence/Game/beam tower component.hpp
@@ -0,0 +1,14 @@
+//
+//  beam tower component.hpp
+//  Classic Tower Defence
+//
+//  Created by Indi Kernick on 5/5/18.
+//  Copyright © 2018 Indi Kernick. All rights reserved.
+//
+
+#ifndef beam_tower_component_hpp
+#define beam_tower_component_hpp
+
+struct BeamTower {};
+
+#endif
---
+++ Classic Tower Defence/Game/projectile tower component.hpp
@@ -0,0 +1,14 @@
+//
+//  projectile tower component.hpp
+//  Classic Tower Defence
+//
+//  Created by Indi Kernick on 5/5/18.
+//  Copyright © 2018 Indi Kernick. All rights reserved.
+//
+
+#ifndef projectile_tower_component_hpp
+#define projectile_tower_component_hpp
+
+struct ProjectileTower {};
+
+#endif
